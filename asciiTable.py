922b4cdd697c70589b850e551bf4b1f35e64d334
saniovincent97 <svinc26@hotmail.com>
2016-08-24 22:31:34 +1000

    Add get_number function into asciiTable program

--- asciiTable.py
+++ asciiTable.py
@@ -1,11 +1,29 @@
-LOWER = 33
-UPPER = 127
-string_to_convert = (input("Please enter a character"))
-print("The ASCII code for ", string_to_convert + " is", ord(string_to_convert))
-num_to_convert = int(input("Enter a number between {} and {}".format(LOWER, UPPER)))
-if num_to_convert > LOWER and num_to_convert < UPPER:
-    print("The ASCII code for ", num_to_convert,  chr(num_to_convert))
-else:
-    print("Please enter the correct range between {} and {}".format(LOWER, UPPER))
+def main():
+    lower = 33
+    upper = 127
+    string_convert = (input("Please enter a character"))
+    print("The ASCII code for ", string_convert + " is", ord(string_convert))
+    check_num = get_number(lower, upper,)
+    print("The ASCII code for ", check_num, "is" ,chr(check_num) )
 
 
+
+def get_number(lower,upper):
+    while True:
+        try:
+            num_convert = int(input("Enter a number between {} and {}".format(lower, upper)))
+            while num_convert > lower and num_convert < upper:
+                return num_convert
+            else:
+                print("Please enter a valid number!")
+                continue
+        except ValueError:
+            print("Please enter a valid number!")
+        else:
+            break
+
+
+
+
+
+main()

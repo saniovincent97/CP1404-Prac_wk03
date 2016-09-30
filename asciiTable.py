b382be41f9aa3ea5dd84b6e567f72a6c2018d29e
saniovincent97 <svinc26@hotmail.com>
2016-09-30 13:06:20 +1000

    Update to last few weeks of work

--- asciiTable.py
+++ asciiTable.py
@@ -12,15 +12,13 @@ def get_number(lower,upper):
     while True:
         try:
             num_convert = int(input("Enter a number between {} and {}".format(lower, upper)))
-            while num_convert > lower and num_convert < upper:
+            if num_convert >= lower and num_convert <= upper:
                 return num_convert
             else:
                 print("Please enter a valid number!")
-                continue
         except ValueError:
             print("Please enter a valid number!")
-        else:
-            break
+
 
 
 
